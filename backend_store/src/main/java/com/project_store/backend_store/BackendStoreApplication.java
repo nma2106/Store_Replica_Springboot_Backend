51d7b3f04f518f73f412faa31bce57712c6558c0
nma2106 <aryannaman51@gmail.com>
2024-06-01 13:17:48 +0530

    fix:databaseSetup

---
+++ backend_store/src/main/java/com/project_store/backend_store/BackendStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project_store.backend_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStoreApplication.class, args);
+	}
+
+}
---
+++ backend_store/src/main/java/com/project_store/backend_store/TestDataObject.java
@@ -0,0 +1,16 @@
+package com.project_store.backend_store;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class TestDataObject {
+    @Id
+    private  long id;
+
+    @Column
+    private  String username;
+
+}
